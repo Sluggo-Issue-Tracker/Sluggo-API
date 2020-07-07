4110fe073909089acc5351a6d51c4628837ccd51
Samuel Schmidt <tdimhcsleumas@gmail.com>
2020-07-07 16:08:47 -0700

    added rest app

--- sluggoAPI/settings.py
+++ sluggoAPI/settings.py
@@ -102,6 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
