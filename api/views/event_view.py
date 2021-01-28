5e9f994e0c8aec462e77d850ce9777c352c66dde
masonwoodford <mwoodfor@ucsc.edu>
2021-01-28 15:25:47 -0800

    basic viewset completed

--- api/views/event_view.py
+++ api/views/event_view.py
@@ -13,18 +13,52 @@ from ..serializers import (
 )
 
 from .team_base import *
-from ..permissions import IsUserOwnerOrReadOnly
 
 class EventViewSet(TeamRelatedViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [
         permissions.IsAuthenticated,
-        IsUserOwnerOrReadOnly | IsAdminMemberOrReadOnly,
+        IsAdminMemberOrReadOnly,
         IsMemberUser,
     ]
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     search_fields = ['^team__name', '^title', '^description', '^description', '^event_type',
                      '^user']
     ordering_fields = ['created', 'team', 'event_type', 'user']
-    
+
+    @action(methods=["GET"],
+            permission_classes=[permissions.IsAuthenticated, IsMemberUser]
+    )
+    def list(self, request, pk=None):
+        serializer = self.serializer_class(self.queryset, many=True)
+        event = get_object_or_404(Event, pk=pk)
+        self.check_object_permissions(request, event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        detail=True,
+        methods=["GET"],
+        permission_classes=[permissions.IsAuthenticated, IsMemberUser]
+    )
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        response = serializer.data
+        return Response(response, status=status.HTTP_200_OK)
+
+    @action(
+        detail=True,
+        methods=["DELETE"],
+        permission_classes=[permissions.IsAuthenticated, IsMemberUser, IsAdminMemberOrReadOnly]
+    )
+    def destroy(self, request, *args, **kwargs):
+        """ deactivate this ticket this is deletion but only to deactivate the record """
+        try:
+            instance = self.get_object()
+            self.check_object_permissions(request, instance)
+            self.perform_destroy(instance)
+            return Response({"msg": "okay"}, status=status.HTTP_200_OK)
+        except Event.DoesNotExist:
+            return Response({"msg": "no such event"}, status=status.HTTP_404_NOT_FOUND)
+
