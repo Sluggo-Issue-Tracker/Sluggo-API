41db54166dc84ff20cd2cc42eb359566a4b66f58
Samuel Schmidt <tdimhcsleumas@gmail.com>
2021-01-28 17:22:57 -0800

    Define list as just calling the base method, but with different permissions. Inherit destroyModelMixin. Add event_view to views/__init__.py. Add viewset to urls.

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,2 +1,3 @@
 from .social_view import MemberViewSet, TeamViewSet
 from .ticket_view import TicketViewSet, TicketCommentViewSet, TicketStatusViewSet, TagViewSet
+from .event_view import EventViewSet
--- api/views/event_view.py
+++ api/views/event_view.py
@@ -1,9 +1,3 @@
-import django_filters.rest_framework
-from django.utils import timezone
-from treebeard import exceptions as t_except
-from django.core.exceptions import SuspiciousOperation
-from django.contrib.auth import get_user_model
-
 from ..models import (
     Event
 )
@@ -14,7 +8,8 @@ from ..serializers import (
 
 from .team_base import *
 
-class EventViewSet(TeamRelatedViewSet, mixins.DeleteModelMixin):
+
+class EventViewSet(TeamRelatedViewSet, mixins.DestroyModelMixin):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [
@@ -22,26 +17,18 @@ class EventViewSet(TeamRelatedViewSet, mixins.DeleteModelMixin):
         IsAdminMemberOrReadOnly,
         IsMemberUser,
     ]
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    search_fields = ['^team__name', '^title', '^description', '^description', '^event_type',
-                     '^user']
-    ordering_fields = ['created', 'team', 'event_type', 'user']
 
-    def list(self, request):
-        serializer = self.serializer_class(self.queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    @action(detail=True, methods=["GET"], permission_classes=[
+        permissions.IsAuthenticated, IsMemberUser
+    ])
+    def list_team(self, request, pk=None):
+        return super().list_team(request, pk)
 
-    @action(
-        detail=True,
-        methods=["DELETE"],
-        permission_classes=[permissions.IsAuthenticated, IsMemberUser, IsAdminMemberOrReadOnly]
-    )
     def destroy(self, request, *args, **kwargs):
         try:
             instance = self.get_object()
             self.check_object_permissions(request, instance)
             self.perform_destroy(instance)
-            return Response({"msg": "okay"}, status=status.HTTP_200_OK)
+            return Response({"msg": "okay"}, status=status.HTTP_204_NO_CONTENT)
         except Event.DoesNotExist:
             return Response({"msg": "no such event"}, status=status.HTTP_404_NOT_FOUND)
-
