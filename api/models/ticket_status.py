769b83366dea02b226db58fae737f4dd3a1e62e1
Samuel Schmidt <tdimhcsleumas@gmail.com>
2021-02-13 17:34:22 -0800

    titles unique to team, not globally

--- api/models/ticket_status.py
+++ api/models/ticket_status.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .team import Team
+from hashlib import md5
 
 from api.models.interfaces import HasUuid, TeamRelated
 
@@ -10,7 +11,10 @@ class TicketStatus(HasUuid, TeamRelated):
     By default, they will be created, started, and completed
     """
 
-    title = models.CharField(max_length=100, unique=True)
+    team_title_hash = models.CharField(max_length=256,
+                                       unique=True,
+                                       editable=False)
+    title = models.CharField(max_length=100, unique=False)
     created = models.DateTimeField(auto_now_add=True)
     activated = models.DateTimeField(null=True, blank=True)
     deactivated = models.DateTimeField(null=True, blank=True)
@@ -21,3 +25,20 @@ class TicketStatus(HasUuid, TeamRelated):
 
     def __str__(self):
         return f"TicketStatus: {self.title}"
+
+    def save(self, *args, **kwargs):
+        team = self.team
+        title = self.title
+
+        if not title:
+            raise ValueError("missing title")
+
+        if not team:
+            raise ValueError("missing team")
+
+        title_id = "{}".format(title)
+        team_id = "{}".format(team.id)
+        self.team_title_hash = md5(title_id.encode()).hexdigest() + md5(
+            team_id.encode()).hexdigest()
+
+        super(TicketStatus, self).save(*args, **kwargs)
