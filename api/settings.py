29c33e42581b4aee3f2001f96163d3bbfd01c5ff
Samuel Schmidt <sadschmi@ucsc.edu>
2021-04-16 07:49:18 -0700

    update datetime format to work with ios

--- api/settings.py
+++ api/settings.py
@@ -122,6 +122,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DATETIME_FORMAT": "%Y-%m-%dT%H:%M:%S%z"
 }
 
 
