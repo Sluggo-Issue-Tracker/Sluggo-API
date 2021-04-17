01e9dcc62a34a62c1a5e495587eceaeb9151c96f
tdimhcsleumas <56523092+tdimhcsleumas@users.noreply.github.com>
2021-04-16 19:17:14 -0700

    Update settings.py
    That sdschmidt guy really needs to watch himself

--- api/settings.py
+++ api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = "adsf:w"
+SECRET_KEY = os.environ.get("SLUGGO_DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
