2365eecc4b4da4206fd9c1cb28caf13b1d3e91c2
Samuel Schmidt <tdimhcsleumas@gmail.com>
2021-08-24 18:00:58 -0700

    updated team list to simple list teams. added separate view to list users' teams

--- api/urls/user_urls.py
+++ api/urls/user_urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import SimpleRouter
 from api.views.user_views.pinned_tickets import UserPinnedTickets
 from api.views.user_views.user_invite import UserInviteViewSet
 from api.views.user_views.assigned_tickets import UserAssignedTickets
+from api.views.user_views.users_teams import UsersTeamsViewSet
 
 
 def buildUserRouterUrls() -> list:
@@ -9,4 +10,5 @@ def buildUserRouterUrls() -> list:
     router.register("pinned-tickets", UserPinnedTickets)
     router.register("assigned-tickets", UserAssignedTickets)
     router.register("invites", UserInviteViewSet)
+    router.register("teams", UsersTeamsViewSet)
     return router.urls
--- api/views/team_related_views/team.py
+++ api/views/team_related_views/team.py
@@ -1,24 +1,15 @@
 from .team_related_base import *
 from api.serializers import *
 from api.permissions import *
-from api.docs import *
 
 
 class TeamViewSet(viewsets.ModelViewSet):
-    queryset = Team.objects.all().prefetch_related("member")
+    queryset = Team.objects.all()
     serializer_class = TeamSerializer
     permission_classes = [IsAdminMemberOrReadOnly, IsMemberUser, IsAuthenticated]
 
     search_fields = ["^name"]
     ordering_fields = ["created", "activated"]
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(
-            self.get_queryset().filter(member__owner=request.user)
-        )
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
