d2800137dee54f09d94d572263ff7cfce4636144
Samuel Schmidt <tdimhcsleumas@gmail.com>
2022-07-24 16:01:09 -0700

    added Dockerfile and modified some settings

--- api/settings.py
+++ api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from constants import DJANGO_SECRET_KEY, JWT_SECRET
+from constants import DJANGO_SECRET_KEY, JWT_SECRET, DEBUG, ALLOWED_HOST
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -26,9 +26,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        "127.0.0.1",
+        "localhost",
+        ALLOWED_HOST,
+]
 
 
 # Application definition
