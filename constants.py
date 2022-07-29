5e8f1619e1629ae69276b93c4f5fbe2f8434c7c8
Samuel Schmidt <tdimhcsleumas@gmail.com>
2022-07-28 22:12:06 -0700

    fix default

--- constants.py
+++ constants.py
@@ -7,6 +7,6 @@ load_dotenv()
 DJANGO_SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 JWT_SECRET = os.environ.get("JWT_SECRET")
 DEBUG = os.environ.get("DEBUG") == "True"
-ALLOWED_HOST = os.environ.get("ALLOWED_HOST", "127.0.0.1")
+ALLOWED_HOST = os.environ.get("ALLOWED_HOST", "http://127.0.0.1")
 BASE_URL = os.environ.get("BASE_URL", "")  # default to empty string
 STATIC_ROOT = os.environ.get("STATIC_ROOT")
