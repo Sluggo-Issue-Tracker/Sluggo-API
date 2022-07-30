4f7dd57cf35988af0230bb15663b2227df1ebdf5
Isaac Trimble-Pederson <wtpisaac@gmail.com>
2022-07-29 23:55:51 -0700

    Nonworking partial commit; need to go review a PR

--- get_database_configuration.py
+++ get_database_configuration.py
@@ -22,39 +22,28 @@ def get_database_configuration(BASE_DIR: str) -> dict:
     elif engine not in valid_engines:
         raise DBConfigError("Invalid engine selected!")
 
-    if engine == "sqlite3":
-        CUSTOM_PATH = os.environ.get("SLUGGO_DB_SQLITE_PATH")
+    # Create dictionary for database configuration with engine (which must
+    # exist)
+    db_dict = {"ENGINE": f"django.db.backends.{engine}"}
 
-        return {
-            "default": {
-                "ENGINE": "django.db.backends.sqlite3",
-                "NAME": CUSTOM_PATH
-                if CUSTOM_PATH is not None
-                else os.path.join(BASE_DIR, "db.sqlite3"),
-            }
-        }
-    else:
-        # Create dictionary for database configuration with engine (which must
-        # exist)
-        db_dict = {"ENGINE": f"django.db.backends.{engine}"}
+    # Configure NAME (behavior will differ based on engine)
 
-        # Get potential fields
-        DB_NAME = os.environ.get("SLUGGO_DB_NAME")
-        DB_USER = os.environ.get("SLUGGO_DB_USER")
-        DB_PASS = os.environ.get("SLUGGO_DB_PASS")
-        DB_HOST = os.environ.get("SLUGGO_DB_HOST")
-        DB_PORT = os.environ.get("SLUGGO_DB_PORT")
+    # Get potential fields
+    DB_USER = os.environ.get("SLUGGO_DB_USER")
+    DB_PASS = os.environ.get("SLUGGO_DB_PASS")
+    DB_HOST = os.environ.get("SLUGGO_DB_HOST")
+    DB_PORT = os.environ.get("SLUGGO_DB_PORT")
 
-        # Add any of these values to their appropriate key if they exist
-        if DB_NAME is not None:
-            db_dict["NAME"] = DB_NAME
-        if DB_USER is not None:
-            db_dict["USER"] = DB_USER
-        if DB_PASS is not None:
-            db_dict["PASS"] = DB_PASS
-        if DB_HOST is not None:
-            db_dict["HOST"] = DB_HOST
-        if DB_PORT is not None:
-            db_dict["PORT"] = DB_PORT
+    # Add any of these values to their appropriate key if they exist
+    if DB_NAME is not None:
+        db_dict["NAME"] = DB_NAME
+    if DB_USER is not None:
+        db_dict["USER"] = DB_USER
+    if DB_PASS is not None:
+        db_dict["PASS"] = DB_PASS
+    if DB_HOST is not None:
+        db_dict["HOST"] = DB_HOST
+    if DB_PORT is not None:
+        db_dict["PORT"] = DB_PORT
 
-        return {"default": db_dict}
+    return {"default": db_dict}
